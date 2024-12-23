f12ada5f076d0159ab29513567cc71f1fb27d592
vinaygaykar <vinay.gaykar+github@proton.me>
2024-12-23 10:07:37 +0530

    (fix) No need to reopen terminal on windows

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -68,8 +68,8 @@ $version = (Get-LatestVersion).Trim()
 $versionDir = Install-KritiCLI -version $version
 Update-PathEnvironment -versionDir $versionDir
 
-Write-Host "=============================================================="
+# Update current session with new path value
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 Write-Host "Kriti CLI version $version installed!"
-Write-Host "CLOSE THIS WINDOW, OPEN NEW ONE AND RUN `kriti` FOR CHANGES TO TAKE EFFECT."
-Write-Host "=============================================================="
+Write-Host "Type `kriti` and hit enter to get started."
 
